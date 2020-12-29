ef2de0ace740f3f8f0ec58b247069f100d7915e0
Gabriel ufot <gabrielufot23@gmail.com>
2020-12-29 08:00:42 +0100

    send_mail()

--- notification.py
+++ notification.py
@@ -1,10 +1,27 @@
-import requests
+import requests     # pulls coingecko ethereum API data
 import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import getpass      # To mask password when enter
 
 
 eth_api_url = 'https://api.coingecko.com/api/v3/coins/ethereum'
+name = input('Enter your name please: ')
+email = input('Enter your email address (gmail please): ')
+password = getpass.getpass()
+send_email_to = input('Enter email address to send to: ')
+alert_amount = input('Alert if Ethereum drops below: ')
 
-answer = requests.get(eth_api_url)
-answer_json = answer.json()
-x = float(answer_json['market_data']['current_price']['usd'])
-print(x)
+
+def send_email():
+    # create message object instance
+    msg = MIMEMultipart()
+
+    # Parameter of the message
+    pwd = password
+    msg['From'] = email
+    msg['To'] = send_email_to
+    msg['Subject'] = 'Present Ethereum Price'
+
+    # incoming message
